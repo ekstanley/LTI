f1d2afb58e8aca0b241131f0e9c5072ccb0cfd96
ekstanley <estanley@ekstanleyholdings.com>
2026-02-01 19:33:33 -0500

    fix(web): remove .eslintrc.js and create standalone ESLint config
    Similar to API package, the .eslintrc.js file was taking precedence over
    .eslintrc.json due to ESLint config file priority (.js > .json).
    
    The .js config extended '@ltip/eslint-config/next' which includes the
    type-checking preset 'plugin:@typescript-eslint/recommended-requiring-type-checking'.
    This preset requires parserOptions.project and causes type-aware linting
    errors in framework-specific code.
    
    Solution:
    - Delete .eslintrc.js to prevent config precedence issues
    - Create standalone .eslintrc.json that:
      * Uses root:true to stop config inheritance
      * Extends only non-type-checking presets
      * Includes Next.js core-web-vitals preset
      * Disables type-aware linting rules globally
    
    Local verification: 0 errors, 3 warnings (img usage + console.log)

--- apps/web/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ['@ltip/eslint-config/next'],
-  parserOptions: {
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-  },
-};
